a8f76017314f998c188829c2497c600862599152
Chase Moffat <chase@MacBook-Pro.local>
2024-02-23 17:20:54 -0500

    Finished lab

---
+++ src/Components/APIForm.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const APIForm = ({ inputs, handleChange, onSubmit }) => {
+  const inputsInfo = [
+    "Input a link to any website you would like to take a screenshot of. Do not include https or any protocol in the URL",
+    "Input which image format you would prefer for your screenshot: jpeg, png, or webp",
+    "Input true or false if you would like your website screenshot to not contain any ads",
+    "Input true or false if you would like your website screenshot to not contain of those annoying 'allow cookies' banners",
+    "Choose the width of your screenshot (in pixels)",
+    "Choose the height of your screenshot (in pixels)",
+  ];
+
+  return (
+    <div>
+      <h2>Select Your Image Attributes:</h2>
+      <form className="form-container" onSubmit={(e) => { e.preventDefault(); onSubmit(); }}>
+        {inputs &&
+          Object.entries(inputs).map(([category, value], index) => (
+            <li className="form" key={index}>
+              <label htmlFor={category}><h2>{category}</h2></label>
+              <input
+                type="text"
+                id={category}
+                name={category}
+                value={value}
+                placeholder="Input this attribute..."
+                onChange={handleChange}
+                className="textbox"
+              />
+              <br />
+              <p>{inputsInfo[index]}</p>
+            </li>
+          ))}
+        <button type="submit" className="button">
+          Take that Pic! 🎞
+        </button>
+        </form>
+    </div>
+  );
+};
+
+export default APIForm;
